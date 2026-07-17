5ebccf03d571a5f49ce3669c7aea8e5676ae0695
Dur-e-Addan <addandure6@gmail.com>
2026-07-17 19:25:29 +0500

    expense_tracker.py

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,12 +1,26 @@
 expenses = []
 items = []
-total_items = int(input("Enter the total number of items you bought:"))
+
+while True:
+    try:
+        total_items = int(input("Enter the total number of items you bought:"))
+        break
+    except ValueError:
+        print("Please enter a valid whole number.")
+
 for i in range(total_items):
-    user_price = int(input("Enter amount spend:"))
+    while True:
+        try:
+            user_price = float(input("Enter amount spend:"))
+            break
+        except ValueError:
+            print("Please enter a valid number for price.")
+    
     user_item = str(input("Enter item bought:"))
     expenses.append(user_price)
     items.append(user_item)
+
 total_amount = sum(expenses)
 for i in range(total_items):
-    print(f'{items[i]}: Rs.{expenses[i]}')
-print(f' You spent a total of {total_amount} Rupees.')
+    print(f'{items[i]}: {expenses[i]}')
+print(f' You spent a total of {total_amount} Rupees')
