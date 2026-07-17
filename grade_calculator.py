21cd8c8b0499061e7e88629d1aa075fa6a3b1ec1
Dur-e-Addan <addandure6@gmail.com>
2026-07-17 20:07:44 +0500

    grade_calculator.py

--- grade_calculator.py
+++ grade_calculator.py
@@ -1,11 +1,26 @@
 subjects = []
 marks = []
-number_sub = int(input("Enter the number of subjects you took:"))
+while True:
+
+    try:
+        number_sub = int(input("Enter the number of subjects you took:"))
+        break
+    except ValueError:
+        print("Please enter a valid number.")
 for i in range(number_sub):
-     user_sub = str(input("Enter the name of subject:"))
-     subjects.append(user_sub)
-     user_marks = int(input("Enter marks obtained out of 100:"))
-     marks.append(user_marks)
+    user_sub = str(input("Enter the name of subject:"))
+    subjects.append(user_sub)
+    while True:
+        try:
+            user_marks = int(input("Enter marks obtained out of 100:"))
+        except ValueError:
+            print("Enter a valid number.")
+            continue
+        if user_marks > 100 or user_marks < 1:
+            print("Marks must be between 0 and 100")
+        else:
+            marks.append(user_marks)
+            break
 for i in range(number_sub):
     print(f'{subjects[i]} : {marks[i]}')
 total_marks = sum(marks)
